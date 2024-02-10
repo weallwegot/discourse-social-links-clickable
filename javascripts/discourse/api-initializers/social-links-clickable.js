aa9c46c1ac7238de744909f9401c510b6b86d1dc
Arkshine <github@arkshine.me>
2024-02-10 03:21:32 +0100

    DEV: Bump API version to reflect renderInOutlet usage

--- javascripts/discourse/api-initializers/social-links-clickable.js
+++ javascripts/discourse/api-initializers/social-links-clickable.js
@@ -1,7 +1,7 @@
 import { apiInitializer } from "discourse/lib/api";
 import SocialLinksClickable from "../components/social-links-clickable";
 
-export default apiInitializer("0.8", (api) => {
+export default apiInitializer("1.13.0", (api) => {
   api.renderInOutlet("user-post-names", SocialLinksClickable);
   api.renderInOutlet("user-card-post-names", SocialLinksClickable);
 
