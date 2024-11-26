0524cd404b3c5d18a15a05ccad2a0e509ff27772
bluefroguk1 <62796282+bluefroguk1@users.noreply.github.com>
2024-11-26 21:35:39 +0000

    Update links-settings.js

--- javascripts/discourse/services/links-settings.js
+++ javascripts/discourse/services/links-settings.js
@@ -2,7 +2,7 @@ import Service from "@ember/service";
 
 export default class SocialLinksClickable extends Service {
   get defaultSettings() {
-    return [
+      return [
       {
         name: "Email",
         icon: "envelope",
