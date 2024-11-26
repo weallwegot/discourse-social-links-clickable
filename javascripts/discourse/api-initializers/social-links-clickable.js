695954f4253f56aebf93db9d62d67ba9a7131748
bluefroguk1 <62796282+bluefroguk1@users.noreply.github.com>
2024-11-26 21:23:02 +0000

    Update social-links-clickable.js

--- javascripts/discourse/api-initializers/social-links-clickable.js
+++ javascripts/discourse/api-initializers/social-links-clickable.js
@@ -2,11 +2,15 @@ import { apiInitializer } from "discourse/lib/api";
 import SocialLinksClickable from "../components/social-links-clickable";
 
 export default apiInitializer("1.13.0", (api) => {
+  // Render the SocialLinksClickable component in the specified outlets
   api.renderInOutlet("user-post-names", SocialLinksClickable);
   api.renderInOutlet("user-card-post-names", SocialLinksClickable);
 
+  // Modify the user-card-contents component to include a custom property
   api.modifyClass("component:user-card-contents", {
     pluginId: "social-links-clickable",
+
+    // Adding a custom property to the user-card-contents component
     hasLocationOrWebsite: true,
   });
 });
